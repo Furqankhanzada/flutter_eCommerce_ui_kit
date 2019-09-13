5299934e1f4fcdabe7842fdb098c65ccf829b86a
zubairkhanzada <zubairkhanzada7@gmail.com>
2019-09-13 17:10:35 +0500

    Implemented DropdownButton in Search page

--- lib/shop/search.dart
+++ lib/shop/search.dart
@@ -6,6 +6,7 @@ class ShopSearch extends StatefulWidget {
 }
 
 class _ShopSearchState extends State<ShopSearch> {
+  String dropdownValue = 'One';
   @override
   RangeValues _values = RangeValues(0.3, 0.7);
   Widget build(BuildContext context) {
@@ -21,9 +22,42 @@ class _ShopSearchState extends State<ShopSearch> {
           ]),
       child: Column(
           children: [
+            Container(
+              margin: new EdgeInsetsDirectional.only(bottom: 15.0),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: DropdownButton<String>(
+                  isExpanded: true,
+                  value: dropdownValue,
+                  icon: Icon(Icons.keyboard_arrow_down),
+                  style: TextStyle(
+                      color: Colors.black
+                  ),
+                  underline: Container(
+                    height: 1,
+                    color: Colors.grey,
+                  ),
+                  onChanged: (String newValue) {
+                    setState(() {
+                      dropdownValue = newValue;
+                    });
+                  },
+                  items: <String>['One', 'Two', 'Free', 'Four']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  })
+                      .toList(),
+                ),
+              ),
+            ),
             Padding(
-              padding: const EdgeInsets.only(top: 10),
-              child: Text('Select Price Range', style: TextStyle(fontWeight: FontWeight.bold)),
+              padding: const EdgeInsets.only(top: 15),
+              child: Text(
+                  'Select Price Range', style: TextStyle(fontWeight: FontWeight.bold)
+              ),
             ),
             RangeSlider(
               values: _values,
@@ -33,7 +67,36 @@ class _ShopSearchState extends State<ShopSearch> {
                 });
               },
             ),
-          ]
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, top: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    width: 120,
+                    height: 45.0,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(color: Theme.of(context).accentColor),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('USD 0', style: TextStyle(color: Colors.white)),
+                    )
+                  ),
+                  Text('to', style: TextStyle(fontSize: 16, color: Colors.black),),
+                  Container(
+                      width: 120,
+                      height: 45.0,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(color: Theme.of(context).accentColor),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('USD 5000', style: TextStyle(color: Colors.white)),
+                      )
+                  ),
+                ],
+              ),
+            )
+          ],
       ),
     );
   }
