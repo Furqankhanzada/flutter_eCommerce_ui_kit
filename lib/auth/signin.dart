8de4caa02cdbcbd58ae32f94133833912bfe0441
zubairkhanzada <zubairkhanzada7@gmail.com>
2019-10-24 13:18:46 +0500

    Implemented Scroll in SignIn and SignUp Screen

--- lib/auth/signin.dart
+++ lib/auth/signin.dart
@@ -13,80 +13,84 @@ class _SignInState extends State<SignIn> {
   final UserCredential userCredential = UserCredential();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.only(left: 25, right: 25),
+    return Scaffold(
+      body: Center(
         child: Form(
           key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(bottom: 12.0),
-                child: TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please Enter Email or Username';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    setState(() {
-                      userCredential.usernameOrEmail = value;
-                    });
-                  },
-                  decoration: InputDecoration(
-                    hintText: 'Enter Username Or Email',
-                    labelText: 'Email',
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 12.0),
+                    child: TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please Enter Email or Username';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        setState(() {
+                          userCredential.usernameOrEmail = value;
+                        });
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Enter Username Or Email',
+                        labelText: 'Email',
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              TextFormField(
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please Enter Password';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  setState(() {
-                    userCredential.password = value;
-                  });
-                },
-                decoration: InputDecoration(
-                  hintText: 'Enter Password',
-                  labelText: 'Password',
-                ),
-                obscureText: true,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 25.0),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 50,
-                  child: Consumer<AuthBlock>(
-                    builder:
-                        (BuildContext context, AuthBlock auth, Widget child) {
-                      return RaisedButton(
-                        color: Theme.of(context).primaryColor,
-                        textColor: Colors.white,
-                        child: Text('Sign In'),
-                        onPressed: () {
-                          // Validate form
-                          if (_formKey.currentState.validate()) {
-                            // Update values
-                            _formKey.currentState.save();
-                            // Hit Api
-                            auth.login(userCredential);
-                          }
-                        },
-                      );
+                  TextFormField(
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please Enter Password';
+                      }
+                      return null;
                     },
+                    onSaved: (value) {
+                      setState(() {
+                        userCredential.password = value;
+                      });
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Enter Password',
+                      labelText: 'Password',
+                    ),
+                    obscureText: true,
                   ),
-                ),
-              )
-            ],
+                  Padding(
+                    padding: const EdgeInsets.only(top: 25.0),
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child: Consumer<AuthBlock>(
+                        builder:
+                            (BuildContext context, AuthBlock auth, Widget child) {
+                          return RaisedButton(
+                            color: Theme.of(context).primaryColor,
+                            textColor: Colors.white,
+                            child: Text('Sign In'),
+                            onPressed: () {
+                              // Validate form
+                              if (_formKey.currentState.validate()) {
+                                // Update values
+                                _formKey.currentState.save();
+                                // Hit Api
+                                auth.login(userCredential);
+                              }
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
         ),
       ),
--- lib/auth/signup.dart
+++ lib/auth/signup.dart
@@ -14,119 +14,123 @@ class _SignUpState extends State<SignUp> {
   String confirmPassword;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.only(left: 25, right: 25),
+    return Scaffold(
+      body: Center(
         child: Form(
           key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(bottom: 12.0),
-                child: TextFormField(
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please Enter Username';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    setState(() {
-                      user.username = value;
-                    });
-                  },
-                  decoration: InputDecoration(
-                    hintText: 'Enter Username',
-                    labelText: 'Username',
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 12.0),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please Enter Username';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        setState(() {
+                          user.username = value;
+                        });
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Enter Username',
+                        labelText: 'Username',
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 12.0),
-                child: TextFormField(
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please Enter Email Address';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    setState(() {
-                      user.email = value;
-                    });
-                  },
-                  decoration: InputDecoration(
-                    hintText: 'Enter Email',
-                    labelText: 'Email',
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 12.0),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please Enter Email Address';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        setState(() {
+                          user.email = value;
+                        });
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Enter Email',
+                        labelText: 'Email',
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 12.0),
-                child: TextFormField(
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 12.0),
+                    child: TextFormField(
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Please Enter Password';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          setState(() {
+                            user.password = value;
+                          });
+                        },
+                        onChanged: (text) {
+                          user.password = text;
+                        },
+                        decoration: InputDecoration(
+                          hintText: 'Enter Password',
+                          labelText: 'Password',
+                        ),
+                        obscureText: true),
+                  ),
+                  TextFormField(
                     validator: (value) {
                       if (value.isEmpty) {
-                        return 'Please Enter Password';
+                        return 'Please Enter Confirm Password';
+                      } else if (user.password != confirmPassword) {
+                        return 'Password fields dont match';
                       }
                       return null;
                     },
-                    onSaved: (value) {
-                      setState(() {
-                        user.password = value;
-                      });
-                    },
                     onChanged: (text) {
-                      user.password = text;
+                      confirmPassword = text;
                     },
                     decoration: InputDecoration(
-                      hintText: 'Enter Password',
-                      labelText: 'Password',
+                      hintText: 'Enter Same Password',
+                      labelText: 'Confirm Password',
                     ),
-                    obscureText: true),
+                    obscureText: true,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 25.0),
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child: Consumer<AuthBlock>(builder:
+                          (BuildContext context, AuthBlock auth, Widget child) {
+                        return RaisedButton(
+                          color: Theme.of(context).primaryColor,
+                          textColor: Colors.white,
+                          child: Text('Sign Up'),
+                          onPressed: () {
+                            if (_formKey.currentState.validate()) {
+                              _formKey.currentState.save();
+                              // If the form is valid, display a snackbar. In the real world,
+                              // you'd often call a server or save the information in a database.
+                              auth.register(user);
+                            }
+                          },
+                        );
+                      }),
+                    ),
+                  )
+                ],
               ),
-              TextFormField(
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please Enter Confirm Password';
-                  } else if (user.password != confirmPassword) {
-                    return 'Password fields dont match';
-                  }
-                  return null;
-                },
-                onChanged: (text) {
-                  confirmPassword = text;
-                },
-                decoration: InputDecoration(
-                  hintText: 'Enter Same Password',
-                  labelText: 'Confirm Password',
-                ),
-                obscureText: true,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 25.0),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 50,
-                  child: Consumer<AuthBlock>(builder:
-                      (BuildContext context, AuthBlock auth, Widget child) {
-                    return RaisedButton(
-                      color: Theme.of(context).primaryColor,
-                      textColor: Colors.white,
-                      child: Text('Sign Up'),
-                      onPressed: () {
-                        if (_formKey.currentState.validate()) {
-                          _formKey.currentState.save();
-                          // If the form is valid, display a snackbar. In the real world,
-                          // you'd often call a server or save the information in a database.
-                          auth.register(user);
-                        }
-                      },
-                    );
-                  }),
-                ),
-              )
-            ],
+            ),
           ),
         ),
       ),
